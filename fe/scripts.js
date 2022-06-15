5f083a15d713696244dba8c83547d7f2b83bbcb3
CamiloDev1 <201248@ids.upchiapas.edu.mx>
2022-06-14 21:47:20 -0500

    first commit

--- fe/scripts.js
+++ fe/scripts.js
@@ -5,7 +5,6 @@ let total = 0;
 function add(productId,price){
     const product = productList.find(p => p.id === productId);
     product.stock--;
-    
     console.log(productId,price);
     carrito.push(productId);
     total = total + price;
@@ -21,7 +20,7 @@ async function pay(){
             headers:{
                "Content-Type":"application/json"
             }
-       })).json(); 
+       })).json();
     }
     catch{
         window.alert("No hay stock");
@@ -31,7 +30,7 @@ async function pay(){
     total = 0;
     await fetchProducts();
     document.getElementById("checkout").innerHTML = `Pagar $${total}`
-    
+
 }
 
 //......
@@ -40,16 +39,16 @@ function displayProducts(){
     let productsHTML = '';
     productList.forEach(p => {
         let buttonHTML =  `<button class="button-add" onclick="add(${p.id},${p.price})">Agregar</button>`;
-        
+
         if(p.stock <=0){
             buttonHTML =  `<button disabled class="button-add disabled" onclick="add(${p.id},${p.price})">Sin stock</button>`;
         }
-    
+
         productsHTML+=
         `<div class="product-container">
         <h3>${p.name}</h3>
         <img src="${p.image}"/>
-        <h2>${p.price}</h2> 
+        <h2>${p.price}</h2>
         ${buttonHTML}
     </div>`
     });
